355a44d2edbd54b8c152c2ed5004b56ba1abbcfe
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-07-10 22:33:39 +0530

    Project Setup

---
+++ src/app/teacher/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      Teacher Dashboard page
+    </div>
+  );
+};
+
+export default page;
